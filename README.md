9c8461a0e0dffd3479be7b16e0e0d3054d4ee98a
Dmi3y <dmi3y888@gmail.com>
2019-10-01 18:46:43 +0300

    Phone model

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ first project
 pip freeze > requirements.txt
 pip install -r requirements.txt
 python manage.py migrate
+p
--- storage/models.py
+++ storage/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Phone(models.Model):
+    phone_model = models.ForeignKey('storage.PhoneModel', on_delete=models.CASCADE)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    comment = models.CharField(max_length=255)
+    date_release = models.DateTimeField(default=timezone.now)
+    date_create = models.DateTimeField(default=timezone.now)
+    date_modify = models.DateTimeField(default=timezone.now)
