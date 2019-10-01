a39ae9e2ae7eb4e4b36690ee7d82c4d8c8ea45ad
SashaPoraiko <poraiko.alexandr@gmail.com>
2019-10-01 18:49:51 +0300

    phone_model commit

--- storage/models.py
+++ storage/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+
+class PhoneModel(models.Model):
+    brand_choises = [
+        ('APPLE', 'Apple'),
+        ('SAMSUNG', 'Samsung'),
+        ('XIAOMI', 'Xiaomi'),
+    ]
+
+    name = models.CharField(max_length=200)
+    brand = models.CharField(
+        max_length=30,
+        choices=brand_choises
+    )
+    model_year = models.IntegerField(
+        validators=[MaxValueValidator(timezone.now().year),
+                    MinValueValidator(timezone.now().year - 1000)])
+
+    def __str__(self):
+        return self.name
