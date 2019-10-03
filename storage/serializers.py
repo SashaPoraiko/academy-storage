5063ee1a741f2a2c2b89f01fed9b92ae69568e6f
SashaPoraiko <poraiko.alexandr@gmail.com>
2019-10-03 19:23:57 +0300

    hptfix

--- storage/serializers.py
+++ storage/serializers.py
@@ -1,8 +1,18 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
+from .models import Phone
+
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ['url', 'username', 'email', 'is_staff']
+        fields = ['url', 'username', 'email', 'is_staff','id']
+
+
+class PhoneSerializer(serializers.ModelSerializer):
+    author = UserSerializer()
+
+    class Meta:
+        model = Phone
+        fields = ('phone_model', 'author', 'comment', 'date_release', 'date_create', 'date_modify')
--- storage/views.py
+++ storage/views.py
@@ -2,9 +2,15 @@ from django.contrib.auth.models import User
 
 from rest_framework import viewsets
 
-from .serializers import UserSerializer
+from .models import Phone
+from .serializers import UserSerializer, PhoneSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class PhoneViewSet(viewsets.ModelViewSet):
+    queryset = Phone.objects.all()
+    serializer_class = PhoneSerializer
