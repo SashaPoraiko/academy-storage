ae2c48743d75ba96b9aef2375dc49338da38dd52
SashaPoraiko <poraiko.alexandr@gmail.com>
2019-10-03 19:23:57 +0300

    hptfix

--- storage/urls.py
+++ storage/urls.py
@@ -1,6 +1,7 @@
 from rest_framework import routers
 
-from storage.views import UserViewSet
+from .views import UserViewSet, PhoneViewSet
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
+router.register(r'phone', PhoneViewSet)
