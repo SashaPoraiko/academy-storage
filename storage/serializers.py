cabc1726747a433510d65b48ac03b3d3bb14bbd6
SashaPoraiko <poraiko.alexandr@gmail.com>
2019-10-08 17:44:04 +0300

    serializers+

--- storage/serializers.py
+++ storage/serializers.py
@@ -1,18 +1,33 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
-from .models import Phone
+from .models import Phone, Part, PhoneModel
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ['url', 'username', 'email', 'is_staff','id']
+        fields = ('id', 'url', 'username', 'email', 'is_staff')
+
+
+class PhoneModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PhoneModel
+        fields = ('id', 'name', 'brand', 'model_year')
 
 
 class PhoneSerializer(serializers.ModelSerializer):
     author = UserSerializer()
+    phone_model = PhoneModelSerializer()
 
     class Meta:
         model = Phone
-        fields = ('phone_model', 'author', 'comment', 'date_release', 'date_create', 'date_modify')
+        fields = ('id', 'phone_model', 'author', 'comment', 'date_release', 'date_create', 'date_modify')
+
+
+class PartSerializer(serializers.ModelSerializer):
+    phone_models = PhoneModelSerializer(many=True)
+
+    class Meta:
+        model = Part
+        fields = ('id', 'name', 'condition', 'phone_models')
--- storage/views.py
+++ storage/views.py
@@ -2,8 +2,8 @@ from django.contrib.auth.models import User
 
 from rest_framework import viewsets
 
-from .models import Phone
-from .serializers import UserSerializer, PhoneSerializer
+from .models import Phone, Part, PhoneModel
+from .serializers import UserSerializer, PhoneSerializer, PartSerializer, PhoneModelSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -14,3 +14,13 @@ class UserViewSet(viewsets.ModelViewSet):
 class PhoneViewSet(viewsets.ModelViewSet):
     queryset = Phone.objects.all()
     serializer_class = PhoneSerializer
+
+
+class PartViewSet(viewsets.ModelViewSet):
+    queryset = Part.objects.all()
+    serializer_class = PartSerializer
+
+
+class PhoneModelViewSet(viewsets.ModelViewSet):
+    queryset = PhoneModel.objects.all()
+    serializer_class = PhoneModelSerializer
