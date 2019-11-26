5baacc0e2ec80c335b12f4a11c7966eaeea44d86
SashaPoraiko <poraiko.alexandr@gmail.com>
2019-11-26 18:32:52 +0200

    proj update

--- sky_storage/settings.py
+++ sky_storage/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 from datetime import timedelta
 
@@ -110,10 +110,11 @@ WSGI_APPLICATION = 'sky_storage.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+'default': dj_database_url.config(dafault='')
 }
 
 # Password validation
