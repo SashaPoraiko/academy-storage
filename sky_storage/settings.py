67eed12002a9880b76b384593c5ffbcf2e27d96d
SashaPoraiko <poraiko.alexandr@gmail.com>
2019-11-26 18:34:43 +0200

    proj update

--- sky_storage/settings.py
+++ sky_storage/settings.py
@@ -114,7 +114,7 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # }
-'default': dj_database_url.config(dafault='')
+    'default': dj_database_url.config(default='')
 }
 
 # Password validation
