69f8e6337242532ccb1f0c091dcb85f482e82004
SashaPoraiko <poraiko.alexandr@gmail.com>
2019-12-12 18:02:06 +0200

    remove lib

--- sky_storage/urls.py
+++ sky_storage/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from django.conf.urls.static import static
+
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
-from management import views
 from sky_storage import settings
 
 urlpatterns = [
@@ -14,7 +15,5 @@ urlpatterns = [
     path('api/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/auth/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
-if settings.DEBUG:
-    from django.conf.urls.static import static
 
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
